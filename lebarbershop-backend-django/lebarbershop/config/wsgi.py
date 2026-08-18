a615de98536bb67ccddc3f7c3294aff17e93bf37
christophenteme@gmail.com <christophenteme@gmail.com>
2026-08-19 04:14:03 +0530

    Vercel Version

--- lebarbershop-backend-django/lebarbershop/config/wsgi.py
+++ lebarbershop-backend-django/lebarbershop/config/wsgi.py
@@ -1,5 +1,9 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lebarbershop.settings')
+
 application = get_wsgi_application()
+
+# Ajouter cette ligne tout en bas pour Vercel
+app = application
